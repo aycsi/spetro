45f14b92cfc5aa0e84d6a335c47fb1e34ea751dc
aycsi <207523432+aycsi@users.noreply.github.com>
2025-09-14 09:52:58 +0200

    v0.0.2

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="spetro",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-)
