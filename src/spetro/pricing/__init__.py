16c0e96d085068d80d72ebcfc22e683464a1dd41
Aycsi <207523432+aycsi@users.noreply.github.com>
2026-02-19 20:32:11 +0100

    fixes

--- src/spetro/pricing/__init__.py
+++ src/spetro/pricing/__init__.py
@@ -1,5 +1,6 @@
 from .pricer import Pricer
 from .payoffs import *
 from .monte_carlo import MonteCarloPricer
+from .iv import bs_cp, iv_slv
 
-__all__ = ["Pricer", "MonteCarloPricer", "european_call", "european_put", "asian_call", "barrier_call"]
+__all__ = ["Pricer", "MonteCarloPricer", "european_call", "european_put", "asian_call", "barrier_call", "bs_cp", "iv_slv"]
