77c52103cafff1b8af39bae2f26291ba1856a79b
Aycsi <207523432+aycsi@users.noreply.github.com>
2026-02-19 21:00:38 +0100

    version

--- src/spetro/__init__.py
+++ src/spetro/__init__.py
@@ -4,7 +4,7 @@ from .calibration import *
 from .neural import *
 from .io import sv_mdl, ld_mdl, sv_srg, ld_srg
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __all__ = [
     "RoughVolatilityEngine", "RoughBergomi", "RoughHeston",
     "JAXBackend", "TorchBackend", "EulerScheme", "HybridScheme",
